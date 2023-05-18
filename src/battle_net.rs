721bb41d20a61899311fb62c11ab4b6c6f304d20
Rob Wygand <rob.wygand@lookout.com>
2023-05-17 20:16:48 -0700

    prototype of pulling guild registry from bnet

---
+++ src/battle_net.rs
@@ -0,0 +1,53 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct OAuthToken {
+    pub access_token: String,
+    pub token_type: String,
+    pub expires_in: usize,
+}
+
+/// To retrieve a token, you need to provide your client_id and client_secret as well as a region (US, EU, APAC or CN)
+///
+/// ```rust
+/// let token = battle_net_oauth::get_oauth_token("client_id", "client_secret", "region");
+/// ```
+pub async fn get_oauth_token(
+    client_id: &str,
+    client_secret: &str,
+    region: &str,
+) -> Result<OAuthToken, Box<dyn std::error::Error + Send + Sync>> {
+    let client = reqwest::Client::new();
+    let url = if region.to_lowercase() == "cn" {
+        "https://www.battlenet.com.cn/oauth/token".to_string()
+    } else {
+        format!("https://{}.battle.net/oauth/token", region.to_lowercase())
+    };
+
+    let resp: OAuthToken = client
+        .post(&url)
+        .basic_auth(client_id, Some(client_secret))
+        .form(&[("grant_type", "client_credentials")])
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    Ok(resp)
+}
+
+
+async fn get_roster(access_token: String) -> Result<String, reqwest::Error> {
+    let header = format!("Bearer {}", access_token);
+    let url = "https://us.api.blizzard.com/data/wow/guild/proudmoore/power-word-taint/roster?namespace=profile-us&locale=en_US";
+
+    let resp = reqwest::Client::new()
+        .get(url)
+        .header(reqwest::header::AUTHORIZATION, header)
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    Ok(resp)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+mod battle_net;
+
+#[macro_use] extern crate rocket;
+use std::collections::HashMap;
+use rocket_dyn_templates::Template;
+
+#[get("/")]
+fn index() -> Template {
+    let context: HashMap<String, String> =  HashMap::new();
+    Template::render("index", context)
+}
+
+#[get("/roster")]
+async fn roster() -> String {
+    let token = battle_net::get_oauth_token(
+        "f966f44ce65d45fda45d60f480158dab",
+        "r16jKjXVp1WdSPRb6eHnBE6i2jlrnF9L",
+        "us");
+
+    let token = token.await;
+    let x = match token {
+        Ok(t) => {
+            let res = get_roster(t.access_token);
+            let res = res.await;
+            let r = match res {
+                Ok(body) => body,
+                _ => "Error!!".to_string()
+            };
+            r
+        }
+        Err(err) => format!("Error occured {}", err.to_string())
+    };
+
+    x
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![roster])
+        .attach(Template::fairing())
 }
